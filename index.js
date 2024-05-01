148086e0d3f801dee1049222dc0fde74f22a2bf6
Alejandra <amorales@1aauto.com>
2024-05-01 10:00:51 -0600

    Contact Phase I

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 import express from 'express';
-
+import nodemailer from 'nodemailer';
 const app = express();
 const port = 3000;
-import path from 'path';
-import { fileURLToPath } from 'url';
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+ res.render("main");
+});
+
+app.get('/about', (req, res) => {
+    res.render("about.ejs");
 });
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
+
+app.post("/send_email", function(req, response){
+
+ 
+
+    const transporter = nodemailer.createTransport({
+        
+          port: 587,
+  secure: false,
+        service :'gmail', 
+        auth:{
+        user: 'alejandramoralesestrada@gmail.com',
+        pass:'gtud tdts egyj gmcj'
+     }
+
+    });
+
+    // async..await is not allowed in global scope, must use a wrapper
+async function main() {
+    const from = req.body.email;
+    const name = req.body.name;
+    const message = req.body.message;
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+        from:'contact@route2comply.com' , 
+        to: 'alejandramoralesestrada@gmail.com', 
+        subject:`Contact request from ${name}` , 
+        text:
+        ` Client email  ${from} 
+          Client name  ${name} 
+          Message:  ${message} `
+    });
+  
+    console.log("Message sent: %s", info);
+  }
+  
+  main().catch(console.error);
+  
+
+})
